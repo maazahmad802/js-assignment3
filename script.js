05a046aa73f7645f7e53c0c431f8c8cff309773c
maazahmad802 <maazch2020@gmail.com>
2023-02-18 20:31:53 +0500

    js assignment 3

---
+++ script.js
@@ -0,0 +1,87 @@
+//1- Declare a variable called myVar and assign it the value of 5. Then, using the console.log function, print the value of myVar to the console.
+
+// var myVar;
+// myVar=5;
+// console.log(myVar);
+
+// 2- Declare a variable called myString and assign it the value of "Hello, world!". Then, using the alert function, display the value of myString in an alert box.
+// let myString = "Hello World";
+// alert(myString);
+
+
+// 3- Declare two variables called num1 and num2, and assign them the values of 5 and 10, respectively. Using the prompt function, ask the user to enter a number, and store the result in a variable called userNum. Then, using the console.log function, print the sum of num1, num2, and userNum to the console.
+
+// 4- Declare a variable called myBoolean and assign it the value of true. Using an if statement, check if myBoolean is true, and if so, use the console.log function to print "It's true!" to the console. Otherwise, print "It's false!" to the console.
+
+// let myBoolean=true;
+// if (myBoolean === true) {
+//    console.log("it's true");
+// }
+// else{
+//    console.log("it's false");
+// }
+
+// 5- Use an if/else statement to check if a number is even or odd. If the number is even, log "even" to the console. If the number is odd, log "odd" to the console.
+
+// let number = parseInt(prompt("Enter number to check"));
+// if (number%2 == 0) {
+//    console.log(  number + "Number is even" );
+// }
+// else{
+//    console.log( number + "Number is odd" );
+// }
+
+// 6- Use the + operator to concatenate two strings together. Then, log the result to the console.
+
+// let strng1,strng2 ,combine;
+// strng1="Hey it's me";
+// strng2=" Maaz Ahmad.";
+// combine=strng1+ strng2;
+// console.log(combine);
+
+// 7- Use the * operator to multiply two numbers together. Then, log the result to the console.
+//  let num1,num2,ansNum;
+//  num1=12;
+//  num2=34;
+//  ansNum=num1*num2;
+//  console.log(ansNum);
+
+// 8- Write a program that takes a string from the user using prompt and checks if it's equal to "password". If it is, display an alert saying "Access granted", otherwise display an alert saying "Access denied".
+
+
+// let userInput = prompt("Enter the password:");
+
+// if (userInput === "password") {
+//   alert("Access granted");
+// } else {
+//   alert("Access denied");
+// }
+
+// 9- Write a program that takes three numbers as input and returns the largest of those three numbers.
+
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+// let num3 = parseFloat(prompt("Enter the third number:"));
+
+// let largest = num1;
+
+// if (num2 > largest) {
+//   largest = num2;
+// }
+
+// if (num3 > largest) {
+//   largest = num3;
+// }
+
+// console.log("The largest number is: " + largest);
+
+// 10 -Write a program that takes two strings from the user using prompt and checks if they are equal. If they are, display an alert saying "The strings are equal", otherwise display an alert saying "The strings are not equal".
+
+// let str1 = prompt("Enter the first string:");
+// let str2 = prompt("Enter the second string:");
+
+// if (str1 === str2) {
+//   alert("The strings are equal");
+// } else {
+//   alert("The strings are not equal");
+// }
